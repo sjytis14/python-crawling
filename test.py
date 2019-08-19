9837863f4e93d2c2db1ac7ad31f41fd9a78e5198
sjytis14 <44135024+sjytis14@users.noreply.github.com>
2019-08-19 19:22:56 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome('C:/Users/YunTaek/Downloads/chromedriver_win32/chromedriver.exe')
+driver = webdriver.Chrome('')
 driver.get('https://news.naver.com/main/ranking/popularDay.nhn?rankingType=popular_day&sectionId=103&date=20190526')
 data = []
 time.sleep(2)
